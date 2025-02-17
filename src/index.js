cb59d13286f930b9f005057621cad4ea44866c2b
avalle <a.valleg@ariadnagrid.com>
2025-02-17 13:28:15 +0100

    checkitems 6

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ window.TrelloPowerUp.initialize({
       console.log("✅ El botón de la Power-Up debería aparecer ahora...");
       return [
         {
-          text: 'CheckItems2',
+          text: 'CheckItems3',
           callback: function (t) {
             console.log("📌 Botón presionado, abriendo modal...");
             // Usamos una URL relativa para GitHub Pages; asegúrate de que table.html esté en la raíz
             return t.modal({
-              title: 'CheckItems2',
+              title: 'CheckItems3',
               url: 'https://amaiavg.github.io/trello-power-up-assignments/src/checkItems.html',
               fullscreen: true
             });
